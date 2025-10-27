f67a5b9f6890a2bc3b630127c122ad0b261ff9ff
Spencer-6 <sdehart@usca.edu>
2025-10-27 13:13:46 -0400

    ups

--- js/quiz1.js
+++ js/quiz1.js
@@ -9,3 +9,18 @@ var num3 = Math.floor(Math.random() * 10);
 document.getElementById('F').textContent = num3.toString();
 
 document.querySelector('input[value="4"]').nextSibling.nodeValue=num1.toString() + "-" + num2.toString() + "+" + num3.toString();
+
+
+
+function changeStyle(){
+    event.preventDefault();
+    var classname = this.getAttribute('class');
+    classname == "answer selected" ? this.setAttribute('class', "answer"): this.setAttribute('class', "answer selected");
+    /*if (classname == "answer selected"){
+        this.children[0].children[0].checked = false;
+    }*/
+    
+}
+for (var i = 0; i<4; i++){
+document.getElementsByClassName('answer')[i].addEventListener('click',changeStyle,false);
+}
